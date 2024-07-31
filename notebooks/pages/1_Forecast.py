674f072b9cbe3ac19e8a12792bd0a8998d105e38
LauJoeYing <joeying0712@gmail.com>
2024-07-31 16:29:55 +0800

    Update 1_Forecast.py

--- notebooks/pages/1_Forecast.py
+++ notebooks/pages/1_Forecast.py
@@ -131,24 +131,28 @@ with col2:
 
         # Apply transformation pipeline to input data
         input_data_transformed = full_pipeline.fit_transform(input_data)
-        st.write(f"Transformed Data: {input_data_transformed}") # Ensure to fit_transform on input data
-
+       
         # Repeat input_data to match time steps expected by the model
         input_data_transformed = np.repeat(input_data_transformed, time_steps, axis=0).reshape((1, time_steps, -1))
 
         # Generate prediction
         prediction_scaled = transformer_model.predict(input_data_transformed).flatten()[0]
-        st.write(f"Scaled Prediction: {prediction_scaled}")
 
         min_meter_reading = scaler.data_min_[0]
         max_meter_reading = scaler.data_max_[0]
-        st.write(f"Min Value: {min_meter_reading}")
-        st.write(f"Max Value: {max_meter_reading}")
 
         # Convert the prediction back to the original scale
         prediction_original = prediction_scaled * (max_meter_reading - min_meter_reading) + min_meter_reading
-        st.write(f"Actual Prediction: {prediction_original}")
-        st.write(f"Deployed Model: Transformer Model")
+
+        # Create a DataFrame to display the results in a table
+        prediction = {
+            'Metric': ['Transformed Data', 'Scaled Prediction', 'Min Value', 'Max Value', 'Actual Prediction'],
+            'Value': [input_data_transformed.flatten(), prediction_scaled, min_meter_reading, max_meter_reading, prediction_original]
+        }
+
+        prediction_df = pd.DataFrame(prediction)
+        st.table(prediction_df)
+
 
         data = {
             'Model': ['Transformer'],
@@ -199,7 +203,7 @@ with col2:
         if 'wind_direction' in shap_values_df.index:
             shap_values_df = shap_values_df.drop('wind_direction')
         st.session_state.shap_values_df = shap_values_df
-        st.write(shap_values_df)
+        st.table(shap_values_df)
 
         # Plot SHAP summary with Plotly
         fig = px.bar(
@@ -218,3 +222,14 @@ with col2:
 
         # Display the plot
         st.plotly_chart(fig, use_container_width=True)
+
+        st.markdown(
+        """
+        <style>
+        .stTable {
+            background-color: white;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+        )
