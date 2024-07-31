afb910b068cfec1b1a4a9819f896c3728485de25
LauJoeYing <joeying0712@gmail.com>
2024-07-31 11:13:18 +0800

    Update 5_Energy_Cost_Calculator.py

--- notebooks/pages/5_Energy_Cost_Calculator.py
+++ notebooks/pages/5_Energy_Cost_Calculator.py
@@ -28,8 +28,8 @@ st.markdown("""
         margin: 5px 0;  /* Adjusted margin to reduce spacing */
         font-size: 0.8em;
         font-family: 'Playwrite DE Grund', cursive;
-        min-width: 800px;
-        max-width: 800px;
+        min-width: 830px;
+        max-width: 830px;
     }
     .custom-table th,
     .custom-table td {
