4a207c7bc0e49b40d5d0597d2562f39e1fcf5f04
LauJoeYing <joeying0712@gmail.com>
2024-07-31 11:07:34 +0800

    Update 5_Energy_Cost_Calculator.py

--- notebooks/pages/5_Energy_Cost_Calculator.py
+++ notebooks/pages/5_Energy_Cost_Calculator.py
@@ -28,7 +28,9 @@ st.markdown("""
         margin: 25px 0;
         font-size: 0.8em;
         font-family: 'Playwrite DE Grund', cursive;
-        min-width: 300px;
+        min-width: 500px;
+        max-width: 500px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
     }
     .custom-table th,
     .custom-table td {
@@ -58,14 +60,6 @@ st.markdown("""
 # Streamlit app layout
 st.markdown("<p class='custom-chatbot-title'>Energy Cost Calculator</p>", unsafe_allow_html=True)
 st.markdown("<div class='rainbow' style='margin-bottom: 2rem;'></div>", unsafe_allow_html=True)
-
-st.info(
-        """
-        This calculator is only a guide and based on normal billing cycle.
-        This bill calculation is meant to calculate energy consumption only, and does not include other charges such as 1% late payment, Power Factor surcharge, Connected Load Charge (CLC) penalty etc.
-        This calculation does not take into account rebates, discounts, or special tariff incentives such as Off Peak Tariff Ride (OPTR), Sunday Tariff Rider (STR) etc*
-        """
-    )
 
 # User inputs for kWh usage
 if 'kWh_usage' not in st.session_state:
@@ -137,4 +131,10 @@ if st.button("Calculate"):
         st.markdown(calculation_df.to_html(index=False, classes="custom-table"), unsafe_allow_html=True)
         st.markdown("</div>", unsafe_allow_html=True)
 
-    
+    st.info(
+        """
+        This calculator is only a guide and based on normal billing cycle.
+        This bill calculation is meant to calculate energy consumption only, and does not include other charges such as 1% late payment, Power Factor surcharge, Connected Load Charge (CLC) penalty etc.
+        This calculation does not take into account rebates, discounts, or special tariff incentives such as Off Peak Tariff Ride (OPTR), Sunday Tariff Rider (STR) etc*
+        """
+    )
